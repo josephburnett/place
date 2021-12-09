61c0bbbcb95676d2024b6e8fe7d192d0bd71fc27
Joseph Burnett <josephburnett79@gmail.com>
2021-12-09 11:33:53 +0100

    Ignore generated files.

--- place.core/resources/public/cljs-out/dev/generated-input-files/gen_test_runner.cljs
+++
@@ -1,5 +0,0 @@
-(ns figwheel.main.generated.dev-auto-test-runner
-  (:require [cljs.test :refer [run-tests]]
-            [cljs-test-display.core] [place.core-test]))
-
-  (run-tests (cljs-test-display.core/init! "app-auto-testing") (quote place.core-test))
